5a4e90338a8af95e962252108e170daa6bf357bb
Kordian Marczak <marczakkordian@gmail.com>
2021-03-03 15:38:53 +0100

    01_06 finishing task

--- 01_types_homework/06.py
+++ 01_types_homework/06.py
@@ -6,10 +6,35 @@
 # Usuń spacje i połącz wszystkie słowa myślnikiem
 # Podziel tekst na osobne zdania za pomocą kropki
 
-string = "The Zen of Python, by Tim Peters\n\nBeautiful is better than ugly.\nExplicit is better than implicit.\nSimple is better than complex.\nComplex is better than complicated.\nFlat is better than nested.\nSparse is better than dense.\nReadability counts.\nSpecial cases aren\'t special enough to break the rules.\nAlthough practicality beats purity.\nErrors should never pass silently.\nnless explicitly silenced.\nIn the face of ambiguity, refuse the temptation to guess.\nThere should be one-- and preferably only one --obvious way to do it.\nAlthough that way may not be obvious at first unless you\'re Dutch.\nNow is better than never.\nAlthough never is often better than *right* now.\nIf the implementation is hard to explain, it\'s a bad idea.\nIf the implementation is easy to explain, it may be a good idea.\nNamespaces are one honking great idea -- let\'s do more of those!"
-print(string)
-print(type(string))
-print(len(string))
+s: str = "The Zen of Python, by Tim Peters\n\nBeautiful is better than ugly.\nExplicit is better than " \
+    "implicit.\nSimple is better than complex.\nComplex is better than complicated.\nFlat is better than " \
+    "nested.\nSparse is better than dense.\nReadability counts.\nSpecial cases aren\'t special enough to break " \
+    "the rules.\nAlthough practicality beats purity.\nErrors should never pass silently.\nUnless explicitly " \
+    "silenced.\nIn the face of ambiguity, refuse the temptation to guess.\nThere should be one-- and preferably " \
+    "only one --obvious way to do it.\nAlthough that way may not be obvious at first unless you\'re Dutch.\nNow " \
+    "is better than never.\nAlthough never is often better than *right* now.\nIf the implementation is hard to " \
+    "explain, it\'s a bad idea.\nIf the implementation is easy to explain, it may be a good idea.\nNamespaces " \
+    "are one honking great idea -- let\'s do more of those! "
+print(s)
+print(type(s))
+print(len(s))
 
-counter = string.count("better")
-print(counter)
+# 1 count appearance of 'better'
+counter = s.count('better')
+print('#1', 'better', counter)
+
+# 2 remove '*' character
+s_2 = s.replace('*', '')
+print('#2', s_2, '*', counter)
+
+# 3 change one occurrence of 'explain' to 'understand'
+s_3 = s.replace('explain', 'understand', 1)
+print('#3', s_3)
+
+# 4 remove spaces and replace them '-' character
+s_4 = s.replace(' ', '-')
+print('#4', s_4)
+
+# 5 split text using '.' separator
+s_5 = s.split('.')
+print('#5', s_5)
