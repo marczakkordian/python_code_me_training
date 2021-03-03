024c8933b8d7eef0395054651e7354bde272488c
Kordian Marczak <marczakkordian@gmail.com>
2021-03-03 16:04:04 +0100

    auto-refactor

--- 02_flow/classes_notes.py
+++ 02_flow/classes_notes.py
@@ -1,13 +1,12 @@
-#1
+# 1
 # dish = 'gzik'
 # if dish == 'pyzy':
 #     reg = 'Wielkopolska'
 # print(reg)
 
-#2
+# 2
 pa = ""
 magic = "hokuspokus"
 for num in range(2, 10, 2):
     pa = pa + str(num) + magic[num]
     print(pa)
-
