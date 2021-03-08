78676911d4a51eb3bb1e41924831054420a42e8a
Kordian Marczak <marczakkordian@gmail.com>
2021-03-08 21:35:46 +0100

    finishing homework task 02_while

--- 02_flow_homework/02_while/02.py
+++ 02_flow_homework/02_while/02.py
@@ -1,2 +1,14 @@
 # Napisz prostą grę, w której użytkownik musi zgadnąć liczbę od 0 - 20 ukrytą w programie (np. secret_num = 5).
 # Pytaj użytkownika o podanie liczby tak długo, aż nie zgadnie.
+
+# defining secret number
+secret_num = 12
+# defining user number variable
+user_num = int(input('Enter your number (1-20): '))
+
+# loop with guessing number
+while secret_num != user_num:
+    user_num = int(input('Sorry, try again: '))
+
+# condition for winning case
+print('Congrats! You won the game')
