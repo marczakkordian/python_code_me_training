2c052e09ce7f6075810d59f83efb6e4a04fdcd06
Kordian Marczak <marczakkordian@gmail.com>
2021-03-24 19:23:17 +0100

    task 1 and 3

---
+++ 05_files/01.py
@@ -0,0 +1,28 @@
+from random import choice
+
+
+def select_quote(quotes) :
+    quote = choice(quotes)
+    return quote
+
+
+def reformat(quote) :
+    quote_and_author = quote.split(' - ')
+    return quote_and_author
+
+
+def show(quote) :
+    print("Quote of the day is:")
+    print("*" * 50)
+    print(quote[0].center(50))
+    print(quote[1].strip().center(50))
+    print("*" * 50)
+
+
+filename = "quotes.txt"
+with open(filename, 'r', encoding='utf8') as fopen :
+    lines = fopen.readlines()
+
+quote = select_quote(lines)
+quote = reformat(quote)
+show(quote)
