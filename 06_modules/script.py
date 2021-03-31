dc06a5bacc3b4d012c56fb98b4addfae6b3b4f46
Kordian Marczak <marczakkordian@gmail.com>
2021-03-31 18:12:10 +0200

    modules intro

---
+++ 06_modules/script.py
@@ -0,0 +1,6 @@
+from example_module import find_longest_word as flw, text as t
+
+names = ['Laura', 'Nickolas', 'Anna', 'Elizabeth', 'Patrick', 'Vicky']
+
+print(flw(names))
+print(t)
---
+++ 06_modules/shapes_scripts.py
@@ -0,0 +1,5 @@
+import shapes as sh
+
+print(sh.circle(4))
+print(sh.rectangle(4, 5))
+print(sh.triangle(4, 6))
