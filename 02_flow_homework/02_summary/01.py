b8dde503c642a8a365117aaf61bc4d9413aee4c9
Kordian Marczak <marczakkordian@gmail.com>
2021-04-12 22:14:30 +0200

    finished task 01 from 02 summary

--- 02_flow_homework/02_summary/01.py
+++ 02_flow_homework/02_summary/01.py
@@ -1,9 +1,12 @@
 # Pozwól użytkownikowi wprowadzić dowolną liczbę imion ciągiem (np.jako jeden string rozdzielonych przecinkiem lub białym znakiem).
 # Następnie powitaj każdą osobę na liście.
 import re
-specialChars = '@'
-usr_input = str(input('Please enter your names(with separator ";"): '))
-cleaned_input = usr_input.strip().replace(specialChars, '').title().split(';')
 
+# taking data from user and filtering
+usr_input = str(input('Please enter your names(with separator ";"): '))
+cleaned_input = re.sub('[^A-Za-z;]', '', usr_input.strip())
+cleaned_input = cleaned_input.split(';')
+
+# hello loop
 for name in cleaned_input:
     print(f'Hello {name}!')
