50fb3c6c943d31760c6a31e8897f3eec1c267682
Kordian Marczak <marczakkordian@gmail.com>
2021-04-12 23:38:11 +0200

    finished task 02 from 02_summary

--- 02_flow_homework/02_summary/02.py
+++ 02_flow_homework/02_summary/02.py
@@ -1,2 +1,20 @@
 # Pobierz od użytkownika dowolny tekst i wyświetl tylko te znaki, które są na pozycjach parzystych.
 # Wykonaj na dwa sposoby - za pomocą pętli oraz przez sting slicing ( ‘abrakadabra’ -> ‘baaar’).
+import re
+
+usr_input = input('Type your string: ')
+cleaned_input = re.sub('[^A-Za-z]', '', usr_input.strip())
+
+print('String slicing ==>', cleaned_input[1::2])
+
+counter = 1
+str_result = ''
+
+for name in cleaned_input:
+    if counter % 2 == 0:
+        counter = counter + 1
+        str_result = str_result + name
+    else:
+        counter = counter + 1
+
+print(f'Loop result ==> {str_result}')
