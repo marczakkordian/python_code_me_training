c8308c2ac7a0885aef9e385310cd75056cfc6c08
Kordian Marczak <marczakkordian@gmail.com>
2021-04-15 21:14:31 +0200

    03_tuple task 03

--- 03_collections_homework/03_tuple/03.py
+++ 03_collections_homework/03_tuple/03.py
@@ -1,1 +1,7 @@
-# Stwórz krotkę liczb całkowitych. Poproś użytkownika o podanie dowolnej liczby. Jeśli liczba znajduje się na krotce wyswietl jej indeks.
+# Stwórz krotkę liczb całkowitych. Poproś użytkownika o podanie dowolnej liczby.
+# Jeśli liczba znajduje się na krotce wyswietl jej indeks.
+
+num_tuple = 1, 2, 3, 4, 5, 6, 7, 8, 9
+usr_input = int(input('Please enter your number: '))
+
+print(num_tuple.index(usr_input))
