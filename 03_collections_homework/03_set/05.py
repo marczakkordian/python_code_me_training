2fed329cc9c149c545ac33e207ceedb7728390e8
Kordian Marczak <marczakkordian@gmail.com>
2021-04-17 13:14:05 +0200

    03_set task 05

--- 03_collections_homework/03_set/05.py
+++ 03_collections_homework/03_set/05.py
@@ -1,1 +1,16 @@
 # Porównaj zachowanie discard() vs remove() dla typu set.
+input_set = {10, 20, 30, 40, 50, 60, 70}
+
+# remove -- error is raised when element doesn't exist
+
+# input_set.remove(10)
+# print(input_set)
+# input_set.remove(100)
+# print(input_set)
+
+# discard -- no error in case attempt to remove not existing element
+
+# input_set.discard(10)
+# print(input_set)
+input_set.discard(100)
+print(input_set)
