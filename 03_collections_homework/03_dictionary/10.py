c30fefe7307aee0bb078a7629183b437aa31b44a
Kordian Marczak <marczakkordian@gmail.com>
2021-04-18 19:24:13 +0200

    03_dict task 10

--- 03_collections_homework/03_dictionary/10.py
+++ 03_collections_homework/03_dictionary/10.py
@@ -2,3 +2,14 @@
 # jest jej kwadrat (n : n * n).
 # Załóżmy, że użytkownik podał N = 8
 # Wynik: {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
+usr_dict = {}
+usr_numb = int(input('Please enter your number: '))
+
+for i in range(usr_numb):
+    i = i + 1
+    keys = i
+    values = i * i
+    usr_dict[keys] = values
+
+print(usr_dict)
+
