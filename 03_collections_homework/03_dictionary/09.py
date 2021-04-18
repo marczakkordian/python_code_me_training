93f8c50acf70c92473610d17f8c38b7bbca5ac4e
Kordian Marczak <marczakkordian@gmail.com>
2021-04-18 19:02:26 +0200

    03_dict task 09

--- 03_collections_homework/03_dictionary/09.py
+++ 03_collections_homework/03_dictionary/09.py
@@ -1,3 +1,22 @@
 # 5 użytkowników poproś o podanie 4 przedmiotów szkolnych, sprawdź czy przedmioty powtarzają się na listach.
 # Wyświetl najpopularniejszy przedmiot. (Uwzględnij fakt, że użytkownicy mogą zapisać przedmioty małymi,
 # drukowanymi lub zaczynając od dużej litery)
+import re
+import sys
+
+usr_num = 1
+item_table = []
+res_table = []
+
+while usr_num <= 5:
+    usr_input = input(f'User {usr_num} | Type your 4 school items and use semicolon as a separator : ').casefold().strip().split(';')
+    for item in usr_input:
+        item_table.append(item)
+    usr_num = usr_num + 1
+
+for element in item_table:
+    count = item_table.count(element)
+    temp = element, count
+    res_table.append(temp)
+
+print(f'Most popular item with the number of occurrence is {max(res_table, key=lambda x: x[1])}')
